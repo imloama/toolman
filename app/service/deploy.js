994d91f3c4ed7fba688e3fbd4e170079ebabc5af
maxman <imloama@gmail.com>
2020-01-12 21:23:58 +0800

    feat: error

--- app/service/deploy.js
+++ app/service/deploy.js
@@ -4,34 +4,38 @@ const { Service } = require('egg')
 
 class DeployService extends Service {
   index (proj) {
-    shell.cd(proj.disk)
+    let res = shell.cd(proj.disk)
+    if(res !== 0){
+      console.log('目录不存在：' + proj.disk)
+      return -1;
+    }
     shell.rm("-rf", proj.code)
     let branch = ''
     if(proj.source.branch){
       branch = '-b ' + proj.source.branch
     }
-    let res = shell.exec(`git clone ${branch} ${proj.source.repo} ${proj.code}`).code
+    res = shell.exec(`git clone ${branch} ${proj.source.repo} ${proj.code}`).code
     if(res !== 0){
       shell.exit(1)
-      return -1
+      return -2
     }
     shell.cd(proj.code)
     let bs = proj.scripts.build
     if(bs){
       res = shell.exec(bs).code
-      if(res!==0)return -2//编译失败
+      if(res!==0)return -3//编译失败
     }
     let cp = proj.scripts.copy
     if(cp){
       res = shell.exec(cp).code
-      if(res!==0)return -3//复制失败
+      if(res!==0)return -4//复制失败
     }
     shell.cd(proj.target)
     let stop = proj.scripts.stop
     res = shell.exec(stop).code
-    if(res!==0)return -4
+    if(res!==0)return -5
     let start = proj.scripts.start
-    if(shell.exec(start).code!==0)return -5
+    if(shell.exec(start).code!==0)return -6
     shell.echo(`部署${proj.code}成功！`)
     return 0
   }
